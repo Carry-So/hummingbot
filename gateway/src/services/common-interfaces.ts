26f93606b74508dc1949d17126cb0590b6b2c90c
Daniel Liu <liudaniel@qq.com>
2022-10-25 16:37:56 +0800

    refactor(gateway): create globiance connector

--- gateway/src/services/common-interfaces.ts
+++ gateway/src/services/common-interfaces.ts
@@ -45,6 +45,12 @@ import {
   Fraction as XsswapFraction
 } from 'xsswap-sdk';
 import {
+  Token as TokenGlobiance,
+  CurrencyAmount as CurrencyAmountGlobiance,
+  Trade as TradeGlobiance,
+  Fraction as GlobianceFraction
+} from 'globiance-sdk';
+import {
   Trade as SushiswapTrade,
   Token as SushiToken,
   CurrencyAmount as SushiCurrencyAmount,
@@ -105,6 +111,7 @@ export type Tokenish =
   | UniswapCoreToken
   | TokenQuickswap
   | TokenXsswap
+  | TokenGlobiance
   | TokenTraderjoe
   | UniswapCoreToken
   | SushiToken
@@ -127,6 +134,7 @@ export type UniswapishTrade =
   | UniswapV3Trade<Currency, UniswapCoreToken, TradeType>
   | TradeQuickswap
   | TradeXsswap
+  | TradeGlobiance
   | TradeTraderjoe
   | SushiswapTrade<SushiToken, SushiToken, SushiTradeType>
   | UniswapV3Trade<Currency, UniswapCoreToken, TradeType>
@@ -150,6 +158,7 @@ export type UniswapishAmount =
   | CurrencyAmountPangolin
   | CurrencyAmountQuickswap
   | CurrencyAmountXsswap
+  | CurrencyAmountGlobiance
   | UniswapCoreCurrencyAmount<Currency>
   | CurrencyAmountTraderjoe
   | SushiCurrencyAmount<SushiCurrency | SushiToken>
@@ -163,6 +172,7 @@ export type Fractionish =
   | PangolinFraction
   | QuickswapFraction
   | XsswapFraction
+  | GlobianceFraction
   | TraderjoeFraction
   | SushiFraction
   | DefikingdomsFraction
